45169f7fc8bf46c6708aded7704e406978785e8f
Marolboron <marolboron@ya.ru>
2023-01-26 19:04:25 +0300

    Добавлен конфиг

--- library-app/cmd/app/index.js
+++ library-app/cmd/app/index.js
@@ -1,5 +1,6 @@
+const config = require( '../../config' );
 const server = require('../../src/infrastructure/webserver/server')
 
-server.listen(3000, () => {
-  console.log('Listening');
+server.listen(config.PORT, config.HOST, () => {
+  console.info('Listening at ' + config.PORT + ' port');
 })
